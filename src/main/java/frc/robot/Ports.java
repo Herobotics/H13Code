da1d8f370a9f2cbd70108f658fedca51b9dbf9c6
computer2 <Herobotics2500@gmail.com>
2020-02-08 15:45:19 -0600

    Fixed port number

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -4,8 +4,8 @@ public class Ports {
     public static final int chassis_left_1 = 0;
     public static final int chassis_left_2 = 1;
 
-    public static final int chassis_right_1 = 0;
-    public static final int chassis_right_2 = 1;
+    public static final int chassis_right_1 = 2;
+    public static final int chassis_right_2 = 3;
 
     
 }
